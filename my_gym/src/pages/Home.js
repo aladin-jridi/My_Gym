9e46f6ba43b8a1607e6a68a35b11964c50a89316
aladin-jridi <aladinjridi007@gmail.com>
2022-06-28 09:58:35 +0100

    done withe banner style

--- my_gym/src/pages/Home.js
+++ my_gym/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+// import { useState } from "react"
 import React  from 'react'
 import { Box } from "@mui/system"
 import HeroBanner from "../components/HeroBanner"
