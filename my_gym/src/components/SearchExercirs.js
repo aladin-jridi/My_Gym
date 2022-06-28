b6fd857652f39b773c169cf4173a833f9ba406bc
aladin-jridi <aladinjridi007@gmail.com>
2022-06-28 09:19:19 +0100

    git in the home page

---
+++ my_gym/src/components/SearchExercirs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SearchExercirs = () => {
+  return (
+    <div>SearchExercirs</div>
+  )
+}
+
+export default SearchExercirs
--- my_gym/src/pages/Home.js
+++ my_gym/src/pages/Home.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useState } from "react"
+import React  from 'react'
+import { Box } from "@mui/system"
+import HeroBanner from "../components/HeroBanner"
+import SearchExercirs from "../components/SearchExercirs"
+import Exercies from "../components/Exercies"
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner/>
+      <SearchExercirs/>
+      <Exercies/>
+    </Box>
   )
 }
 
