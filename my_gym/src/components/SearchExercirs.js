f7c75decc2becfce04d04986e426b54f648ca7b1
aladin-jridi <aladinjridi007@gmail.com>
2022-06-28 11:29:07 +0100

    style withe UI

--- my_gym/src/components/SearchExercirs.js
+++ my_gym/src/components/SearchExercirs.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import React  from 'react';
+import { Box, Button, Stack, TextField, Typography, typography } from "@mui/material";
+import { width } from "@mui/system";
 
 const SearchExercirs = () => {
   return (
-    <div>SearchExercirs</div>
+    <Stack alignItems='center' mt ='37px' justifyContent='center' p='20px'>
+        <Typography fontWeight={700} sx={{
+            fontSize:{lg: '45px', xs: '30px'}}} mb = '50px' textAlign='center'>
+            Awesome Exercises You <br/> Should Know
+        </Typography>
+        <Box position='relative' mb='72px'>
+            <TextField
+            sx={{input: {fontWeight: '700', border: 'none', borderRadius : '5px'},
+                width:{lg: '1170px' , xs: '350px'},
+                  backgroundColor: '#FFF',
+                  borderRadius: '40px'
+                }}
+                height ='76px'
+                value=''
+                onChange={(e)=>{}}
+                placeholder='search Exercises'
+                type='text'
+            />
+        </Box>
+    </Stack>
   )
 }
 
