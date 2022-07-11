f1d5d038ab955e1ac0947a5e503567835b491dae
aladin-jridi <aladinjridi007@gmail.com>
2022-07-11 17:42:51 +0100

    rapid api fix err

--- my_gym/src/utils/fetchData.js
+++ my_gym/src/utils/fetchData.js
@@ -1,14 +1,37 @@
 export const exerciseOptions  = {
   method: 'GET',
+  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY ,
+    'X-RapidAPI-Key': 'e204058d2cmshcf547a262e5524ep1379d2jsnce80ee597d81',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
 
 
-export const fetchData = async (url, options) =>{
+// export const fetchData = async (url, options) =>{
+//   const res = await fetch('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', options);
+//   const data = await res.json();
+//   return data;
+// }
+// export const exerciseOptions = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+//     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+//   },
+// };
+
+// export const youtubeOptions = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+//     'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+//   },
+// };
+
+export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
   const data = await res.json();
+
   return data;
-}
+};
